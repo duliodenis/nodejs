53ea33df458d7ba09d8d47bdc9f65ecc88b2d8bc
Dulio Denis <dulio.denis@yahoo.com>
2015-10-03 15:14:00 -0400

    Simple Web Server using node

---
+++ 01-simpleServer/README.md
@@ -0,0 +1,17 @@
+Simple Server
+==============
+![](https://raw.githubusercontent.com/duliodenis/nodejs/master/art/nodejs.png)
+
+This server is executed with:
+$ npm start
+or 
+$ node server
+
+The package.json manifest file was created with:
+$ npm init
+
+### Licensing
+My nodeJS projects are licensed under [the MIT License](https://github.com/duliodenis/nodejs/blob/master/LICENSE).
+
+### Support or Contact
+Visit [ddApps.co](http://ddapps.co) to see more.
--- 01-simpleServer/server.js
+++ 01-simpleServer/server.js
@@ -5,15 +5,59 @@
 //  Created by Dulio Denis on 10/2/15.
 //  Copyright (c) 2015 ddApps. All rights reserved.
 //  ------------------------------------------------
-//  Code from nodejs about:
+//  Based on code from nodejs about:
 //  https://nodejs.org/en/about/
 //
 
+
+// Required modules
 var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:1337/');
+// Array of Available MIME types
+var mimeTypes = {
+    html:   "text/html",
+    jpeg:   "image/jpeg",
+    jpg:    "image/jpg",
+    png:    "image/png",
+    js:     "text/javascript",
+    css:    "text/css"
+};
+
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), unescape(uri));
+    console.log('Loading ' + uri);
+    
+    var stats;
+    try { 
+        stats = fs.lstatSync(filename);
+    }
+    catch(e) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.write('404 Not Found');
+        res.end();
+        return;
+    }
+    
+    // Determine whether a file or directory
+    if (stats.isFile()) {
+        var mimeType = mimeTypes[path.extname(filename).split('.').reverse()[0]];
+        res.writeHead(200, {'Content-type': mimeType });
+        
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(res);
+        
+    } else if (stats.isDirectory()) {
+        res.writeHead(302, {'Location': 'index.html'});
+        res.end();
+    } else { // its neither a file or a directory
+        res.writeHead(500, {'Location': 'text/plain'});
+        res.write('500: Internal Error\n');
+        res.end();
+    }
+                  
+}).listen(3000);
