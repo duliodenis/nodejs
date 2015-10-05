aa18636b27e613d4a97a9e0e3833f5ac0d3a5868
Dulio Denis <dulio.denis@yahoo.com>
2015-10-05 11:33:20 -0400

    Added Page Routes and Views

--- 02-Express/express-website/routes/contact.js
+++ 02-Express/express-website/routes/contact.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('contact', { title: 'Contact' });
 });
+
+router.post();
 
 module.exports = router;
