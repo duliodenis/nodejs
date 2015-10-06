47f62308b4b2e1cb182d5f2600e8ac8d2adf3fc9
Dulio Denis <dulio.denis@yahoo.com>
2015-10-06 09:04:49 -0400

    Implement nodemailer contact route

--- 02-Express/express-website/routes/contact.js
+++ 02-Express/express-website/routes/contact.js
@@ -1,11 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
 
-router.post();
+// For posting contacting email
+router.post('/send', function(req, res, next) {
+	var transporter = nodemailer.createTransport({
+		service: 	'Gmail',
+		auth: {
+			user: 'account@gmail.com',
+			pass: 'account-password'  
+		}
+	});
+
+	var mailOptions = {
+		from: 	 'john@outlook.com',
+		to: 	 'account@gmail.com',
+		subject: 'Website Submission',
+		text: 	 'You have a new submission with the details of name: ' +req.body.name+ 
+				 'email: ' +req.body.email+ ' Message: ' +req.body.message,
+		html: 	 '<p>You have a new submission with the following details</p> <ul><li>Name: ' +req.body.name+ 
+				 '</li><li>email: ' +req.body.email+ '</li><li> Message: ' +req.body.message+ '</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if (error) { 
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent '+info.response);
+			res.redirect('/');
+		}
+	});
+
+});
 
 module.exports = router;
