4c58e3ac735f84826fc7fd01bb65d137689157dd
Dulio Denis <dulio.denis@yahoo.com>
2015-10-20 12:43:14 -0400

    Simple Bank Account Simulation

--- 03-bankAccount/bankAccount.js
+++ 03-bankAccount/bankAccount.js
@@ -6,14 +6,33 @@
 //  Copyright (c) 2015 ddApps. All rights reserved.
 //  ------------------------------------------------
 //
-var deposit = function() {
 
+var account = {
+	balance: 0
+};
+
+var deposit = function(account, amount) {
+	console.log("Deposit of " + amount);
+	account.balance += amount;
 }
 
-var withdrawal = function() {
-
+var withdrawal = function(account, amount) {
+	console.log("Withdrawal of " + amount);
+	account.balance -= amount;
 }
 
-var getBalance = function() {
-		
+var getBalance = function(account) {
+	return account.balance;
 }
+
+var printBalance = function(account) {
+	console.log("Current Balance: " + getBalance(account));
+}
+ 
+
+// Bank Test Run: deposit $10, Withdraw $5. Result = $5
+printBalance(account);
+deposit(account, 10);
+printBalance(account);
+withdrawal(account, 5);
+printBalance(account);
