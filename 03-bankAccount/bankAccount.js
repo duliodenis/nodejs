c6a431c2bcc8d0436210c88c2df919286428159c
Dulio Denis <dulio.denis@yahoo.com>
2015-10-21 12:03:01 -0400

    Added an Array of Accounts to support multiple account creation

--- 03-bankAccount/bankAccount.js
+++ 03-bankAccount/bankAccount.js
@@ -7,32 +7,67 @@
 //  ------------------------------------------------
 //
 
-var account = {
-	balance: 0
-};
+
+// Array of Accounts
+var accounts = [];
+
+
+// Creates an account and adds it to the array of accounts
+// Returns: an account object
+var createAccount = function(account) {
+	accounts.push(account);
+	return account;
+}
+
+
+// Finds matching accounts using a forEach on the array of accounts
+var getAccount = function(username) {
+	var matchingAccounts = [];
+	accounts.forEach(function(account){
+		if (username === account.username) matchingAccounts.push(account);
+	});
+	return matchingAccounts;
+}
 
 var deposit = function(account, amount) {
 	console.log("Deposit of " + amount);
 	account.balance += amount;
 }
 
+
 var withdrawal = function(account, amount) {
 	console.log("Withdrawal of " + amount);
 	account.balance -= amount;
 }
 
+
 var getBalance = function(account) {
 	return account.balance;
 }
 
+
 var printBalance = function(account) {
-	console.log("Current Balance: " + getBalance(account));
+	console.log("Current Balance: " + getBalance(account));		
 }
  
 
-// Bank Test Run: deposit $10, Withdraw $5. Result = $5
-printBalance(account);
-deposit(account, 10);
-printBalance(account);
-withdrawal(account, 5);
-printBalance(account);
+// Bank Test Run: create an 'A' and 'B' account, deposit $10 into 'A', Withdraw $5 from 'B'. Result = A($10), B(-$5).
+
+var AAccount = createAccount({
+	balance: 0,
+	username: "A"
+});
+
+var BAccount = createAccount({
+	balance: 0,
+	username: "B"
+});
+
+var account = getAccount("A");
+console.log("Matches = " + account.length);
+
+printBalance(AAccount);
+deposit(AAccount, 10);
+printBalance(AAccount);
+withdrawal(BAccount, 5);
+printBalance(BAccount);
